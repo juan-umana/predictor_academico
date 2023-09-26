c9cef7703793fd520193f6ec18161b553826fe6d
Juan D. Umaña <juan.umanacaro@gmail.com>
2023-09-26 13:24:08 -0500

    Fix data_plot
    Fix visualizations and get course and application mode counts

--- data_viz/data_plots.R
+++ data_viz/data_plots.R
@@ -12,22 +12,22 @@ colnames(origin_data) <- tolower(colnames(origin_data))
 colnames(origin_data) <- str_replace(colnames(origin_data),' ','_')
 colnames(origin_data) <- str_replace(colnames(origin_data),"'",'')
 
-origin_data <- origin_data %>% mutate(targe_categ = ifelse(target == "Graduate",
+origin_data <- origin_data %>% mutate(target_categ = ifelse(target == "Graduate",
                                                            "Success", "Fail"))
 
 #### Unidades curriculares aprovadas en primer y segundo semestre ####
 
-units_approved_1_plot <- ggplot(origin_data, aes(x = origin_data$targe_categ,
+units_approved_1_plot <- ggplot(origin_data, aes(x = origin_data$target_categ,
                                                  y = origin_data$`curricular_units 1st sem (approved)`)) + 
-  geom_violin(aes(color=targe_categ), trim = TRUE) +
+  geom_violin(aes(color=target_categ), trim = TRUE) +
   labs(title = "Curricular units approved in 1st semester", y = "Number of units", x = "") + 
   theme_classic() +
   geom_signif(comparisons = list(c("Success", "Fail")),   
               map_signif_level=TRUE)
 
-units_approved_2_plot <- ggplot(origin_data, aes(x = origin_data$targe_categ,
+units_approved_2_plot <- ggplot(origin_data, aes(x = origin_data$target_categ,
                                                  y = origin_data$`curricular_units 2nd sem (approved)`)) + 
-  geom_violin(aes(color=targe_categ), trim = TRUE) +
+  geom_violin(aes(color=target_categ), trim = TRUE) +
   labs(title = "Curricular units approved in 2nd semester", y = "Number of units", x = "") + 
   theme_classic() +
   geom_signif(comparisons = list(c("Success", "Fail")),   
@@ -35,17 +35,17 @@ units_approved_2_plot <- ggplot(origin_data, aes(x = origin_data$targe_categ,
 
 #### Calificación de las unidades curriculares en primer y segundo semestre ####
 
-units_grade_1_plot <- ggplot(origin_data, aes(x = origin_data$targe_categ,
+units_grade_1_plot <- ggplot(origin_data, aes(x = origin_data$target_categ,
                                               y = origin_data$`curricular_units 1st sem (grade)`)) + 
-  geom_violin(aes(color=targe_categ), trim = TRUE) +
+  geom_violin(aes(color=target_categ), trim = TRUE) +
   labs(title = "Grade units in 1st semester", y = "Grade", x = "") + 
   theme_classic() +
   geom_signif(comparisons = list(c("Success", "Fail")),   
               map_signif_level=TRUE)
 
-units_grade_2_plot <- ggplot(origin_data, aes(x = origin_data$targe_categ,
+units_grade_2_plot <- ggplot(origin_data, aes(x = origin_data$target_categ,
                                               y = origin_data$`curricular_units 2nd sem (grade)`)) + 
-  geom_violin(aes(color=targe_categ), trim = TRUE) +
+  geom_violin(aes(color=target_categ), trim = TRUE) +
   labs(title = "Grade units in 2nd semester", y = "Grade", x = "") + 
   theme_classic() +
   geom_signif(comparisons = list(c("Success", "Fail")),   
@@ -56,37 +56,41 @@ units_grade_2_plot <- ggplot(origin_data, aes(x = origin_data$targe_categ,
 course_plot <- ggplot(origin_data,aes(x = course)) + 
   theme_light() +
   labs(title = "Student course", y = "Count", x = "Course") +
-  geom_histogram(data=subset(origin_data, targe_categ == 'Success'),fill = "green", alpha = 0.25) +
-  geom_histogram(data=subset(origin_data, targe_categ == 'Fail'),fill = "red", alpha = 0.25)
+  geom_histogram(data=subset(origin_data, target_categ == 'Success'),fill = "green", alpha = 0.25) +
+  geom_histogram(data=subset(origin_data, target_categ == 'Fail'),fill = "red", alpha = 0.25)
+
+course_counts <- origin_data %>% group_by(target_categ, course) %>% count()
 
 #### Matrículas ####
 
 tuition_plot <- ggplot(origin_data,aes(x = `tuition_fees up to date`)) + 
   theme_light() +
   labs(title = "Tuition", y = "Count", x = "Tuition") +
-  geom_histogram(data=subset(origin_data, targe_categ == 'Success'),fill = "green", alpha = 0.25) +
-  geom_histogram(data=subset(origin_data, targe_categ == 'Fail'),fill = "red", alpha = 0.25)
+  geom_histogram(data=subset(origin_data, target_categ == 'Success'),fill = "green", alpha = 0.25) +
+  geom_histogram(data=subset(origin_data, target_categ == 'Fail'),fill = "red", alpha = 0.25)
 
 #### Scholarship ####
 
 scholarship_plot <- ggplot(origin_data,aes(x = scholarship_holder)) + 
   theme_light() +
   labs(title = "Scholarship", y = "Count", x = "Scholarship") +
-  geom_histogram(data=subset(origin_data, targe_categ == 'Success'),fill = "green", alpha = 0.25) +
-  geom_histogram(data=subset(origin_data, targe_categ == 'Fail'),fill = "red", alpha = 0.25)
+  geom_histogram(data=subset(origin_data, target_categ == 'Success'),fill = "green", alpha = 0.25) +
+  geom_histogram(data=subset(origin_data, target_categ == 'Fail'),fill = "red", alpha = 0.25)
 
 #### Application mode ####
 
 application_plot <- ggplot(origin_data,aes(x = application_mode)) + 
   theme_light() +
   labs(title = "Application", y = "Count", x = "Application") +
-  geom_histogram(data=subset(origin_data, targe_categ == 'Success'),fill = "green", alpha = 0.25) +
-  geom_histogram(data=subset(origin_data, targe_categ == 'Fail'),fill = "red", alpha = 0.25)
+  geom_histogram(data=subset(origin_data, target_categ == 'Success'),fill = "green", alpha = 0.25) +
+  geom_histogram(data=subset(origin_data, target_categ == 'Fail'),fill = "red", alpha = 0.25)
+
+application_counts <- origin_data %>% group_by(target_categ, application_mode) %>% count()
 
 #### Edad al momento de ingreso ####
 
-age_plot <- ggplot(origin_data, aes(x = targe_categ, y = `age_at enrollment`)) + 
-  geom_violin(aes(color=targe_categ), trim = TRUE) +
+age_plot <- ggplot(origin_data, aes(x = target_categ, y = `age_at enrollment`)) + 
+  geom_violin(aes(color=target_categ), trim = TRUE) +
   labs(title = "Age at enrollment", y = "Age", x = "") + 
   theme_classic() +
   geom_signif(comparisons = list(c("Success", "Fail")),   
@@ -94,15 +98,15 @@ age_plot <- ggplot(origin_data, aes(x = targe_categ, y = `age_at enrollment`)) +
 
 #### Unidades curriculares sin evaluación en primer y segundo semestre ####
 
-units_woeval_1_plot <- ggplot(origin_data, aes(x = targe_categ, y = `curricular_units 1st sem (without evaluations)`)) + 
-  geom_violin(aes(color=targe_categ), trim = TRUE) +
+units_woeval_1_plot <- ggplot(origin_data, aes(x = target_categ, y = `curricular_units 1st sem (without evaluations)`)) + 
+  geom_violin(aes(color=target_categ), trim = TRUE) +
   labs(title = "Units without evaluation in 1st semester", y = "Number of units", x = "") + 
   theme_classic() +
   geom_signif(comparisons = list(c("Success", "Fail")),   
               map_signif_level=TRUE)
 
-units_woeval_2_plot <- ggplot(origin_data, aes(x = targe_categ, y = `curricular_units 2nd sem (without evaluations)`)) + 
-  geom_violin(aes(color=targe_categ), trim = TRUE) +
+units_woeval_2_plot <- ggplot(origin_data, aes(x = target_categ, y = `curricular_units 2nd sem (without evaluations)`)) + 
+  geom_violin(aes(color=target_categ), trim = TRUE) +
   labs(title = "Units without evaluation 2nd semester", y = "Number of units", x = "") + 
   theme_classic() +
   geom_signif(comparisons = list(c("Success", "Fail")),   
@@ -110,8 +114,8 @@ units_woeval_2_plot <- ggplot(origin_data, aes(x = targe_categ, y = `curricular_
 
 #### Tasa de inflación ####
 
-inflation_plot <- ggplot(origin_data, aes(x = targe_categ, y = inflation_rate)) + 
-  geom_violin(aes(color=targe_categ), trim = TRUE) +
+inflation_plot <- ggplot(origin_data, aes(x = target_categ, y = inflation_rate)) + 
+  geom_violin(aes(color=target_categ), trim = TRUE) +
   labs(title = "Inflation Rate", y = "Inflation rate", x = "") + 
   theme_classic() +
   geom_signif(comparisons = list(c("Success", "Fail")),   
