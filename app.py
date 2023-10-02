27eb3f8d5eab7b982aa91897b901556b4c7fe4b8
danielaruizl1 <da.ruizl1@uniandes.edu.co>
2023-10-01 20:00:01 -0500

    dashboard local final

--- app.py
+++ app.py
@@ -6,39 +6,27 @@ from dash import html
 import pandas as pd
 import plotly.express as px
 import os
+from model import inference
 
 coolwarm_scale = [[0, 'blue'], [0.5, 'white'], [1, 'red']]
 
-def correlation_plots_dash(correlation_matrix):
+def correlation_plots_dash(correlation_matrix, title):
     fig = px.imshow(correlation_matrix,
                     labels=dict(color="Correlation"),
                     x=correlation_matrix.columns.tolist(),
-                    y=correlation_matrix.index.tolist(),
+                    y=correlation_matrix.columns.tolist(),
                     color_continuous_scale=coolwarm_scale,
                     zmin=-1, zmax=1)
-    
-    #z = correlation_matrix.values
-    #for i in range(z.shape[0]):
-        #for j in range(z.shape[1]):
-            #fig.add_annotation(dict(
-                #x=j,
-                #y=i,
-                #xref='x',
-                #yref='y',
-                #text=str(round(z[i][j], 2)),
-                #showarrow=False,
-                #font=dict(size=10, color='white' if abs(z[i][j]) > 0.5 else 'black')
-            #))
 
-    fig.update_layout(title_text="Correlation Heatmap", xaxis_title="Variables", yaxis_title="Variables")
+    fig.update_layout(title_text=title, xaxis_title="Variables", yaxis_title="Variables")
 
     return fig
 
-contin_correlations =  pd.read_excel(os.path.join("data_exploration","continuos_correlations.xlsx"))
-categ_correlations =  pd.read_excel(os.path.join("data_exploration","categorical_correlations.xlsx"))
+contin_correlations =  pd.read_excel("continuos_correlations.xlsx")
+categ_correlations =  pd.read_excel("categorical_correlations.xlsx")
 
-contin_plot = correlation_plots_dash(contin_correlations)
-categ_plot  = correlation_plots_dash(categ_correlations)
+contin_plot = correlation_plots_dash(contin_correlations, "Mapa de correlaciones entre variables continuas")
+categ_plot  = correlation_plots_dash(categ_correlations, "Mapa de calor de correlaciones entre variables categóricas")
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -58,21 +46,21 @@ app.layout = html.Div(children=[
         A continuación, ingrese los datos correspondientes del estudiante del cual quiere observar su riesgo académico.
     '''),
     html.Div(["Tipo de aplicación: ",
-              dcc.Input(id='var1', value='Ingrese un valor', type='text')]),
+              dcc.Input(id='var1', value='', type='text')]),
     html.Div(["Número de curso: ",
-              dcc.Input(id='var2', value='Ingrese un valor', type='text')]),
+              dcc.Input(id='var2', value='', type='text')]),
     html.Div(["¿El estudiante se encuentra al día con la matricula?: ",
-              dcc.Input(id='var3', value='Si o No', type='text')]),
+              dcc.Input(id='var3', value='', type='text')]),
     html.Div(["¿El estudiante tiene beca?: ",
-              dcc.Input(id='var4', value='Si o No', type='text')]),
+              dcc.Input(id='var4', value='', type='text')]),
     html.Div(["Edad: ",
-              dcc.Input(id='var5', value='Ingrese un valor', type='text')]),
+              dcc.Input(id='var5', value='', type='text')]),
     html.Div(["Nota del estudiante en el primer semestre: ",
-              dcc.Input(id='var6', value='Ingrese un valor', type='text')]),
+              dcc.Input(id='var6', value='', type='text')]),
     html.Div(["Nota del estudiante en el segundo semestre: ",
-              dcc.Input(id='var7', value='Ingrese un valor', type='text')]),
+              dcc.Input(id='var7', value='', type='text')]),
     html.Div(["Tasa de inflación: ",
-              dcc.Input(id='var8', value='Ingrese un valor', type='text')]),    
+              dcc.Input(id='var8', value='', type='text')]),    
     html.Div(id='my-output'),
     ]
 )
@@ -92,8 +80,25 @@ app.layout = html.Div(children=[
 )
 
 def update_output_div(val1, val2, val3, val4, val5, val6, val7, val8):
-    prediction = "MATRICULADO"
+    values = [val1, val2, val3, val4, val5, val6, val7, val8]
+    labels = ['application', 'course', 'tuition', 'scholarship', 'age', 'grade_1', 'grade_2', 'inflation']
+    evidence = {}
+    for i in range(len(values)):
+        if values[i] != '':
+            evidence[labels[i]]=int(values[i])
+    result = inference.query(variables=['success'], evidence=evidence)
+    try:
+        list_result = list(result.values)
+        max_val = list_result.index(max(result.values))
+        if max_val == 0:
+            prediction = 'Deserción'
+        elif max_val == 1:
+            prediction = 'Matriculado'
+        else:
+            prediction = 'Graduado'
+    except:
+        prediction = 'No se puede determinar'
     return 'La predicción del estado estudiantil es: {}'.format(prediction)
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
--- model.py
+++ model.py
@@ -1,9 +1,13 @@
 #%% Librerias
 from pgmpy.estimators import MaximumLikelihoodEstimator
+from pgmpy.inference import VariableElimination
 from pgmpy.estimators import BayesianEstimator
 from pgmpy.sampling import BayesianModelSampling
 from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
+from sklearn.metrics import accuracy_score, confusion_matrix
+import pandas as pd
+from sklearn.model_selection import train_test_split
 import os
 import numpy as np
 
@@ -18,12 +22,7 @@ model_success = BayesianNetwork([('grade_1','grade_2'),('grade_2','success'),
                               ('tuition','success'),
                               ('application','success')])
 
-# Importar librerias
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
 # Subir csv
-# cd Model
 data_model = pd.read_csv('data_model.csv')
 
 #%% Dividir datos en train y test
@@ -35,32 +34,28 @@ emv_success = MaximumLikelihoodEstimator(model = model_success, data = X_train)
 # Obtener las CPDs ajustadas
 cpds = emv_success.get_parameters()
 
-# for cpd in cpds: # No estoy seguro cómo interpretar las tablas
-#     print(cpd)
-
 #%%
+print("Calculando cpds...")
 cpdem_grade_1 = emv_success.estimate_cpd (node = 'grade_1')
-print(cpdem_grade_1)
+#print(cpdem_grade_1)
 cpdem_grade_2 = emv_success.estimate_cpd(node = 'grade_2')
-print(cpdem_grade_2)
+#print(cpdem_grade_2)
 cpdem_age =emv_success.estimate_cpd(node = 'age')
-print(cpdem_age)
+#print(cpdem_age)
 cpdem_inflation = emv_success.estimate_cpd (node = 'inflation')
-print(cpdem_inflation)
+#print(cpdem_inflation)
 cpdem_scholarship = emv_success.estimate_cpd(node = 'scholarship')
-print(cpdem_scholarship)
+#print(cpdem_scholarship)
 cpdem_course = emv_success.estimate_cpd(node = 'course')
-print(cpdem_course)
+#print(cpdem_course)
 cpdem_tuition = emv_success.estimate_cpd(node = 'tuition')
-print(cpdem_tuition)
+#print(cpdem_tuition)
 cpdem_application = emv_success.estimate_cpd(node = 'application')
-print(cpdem_application)
+#print(cpdem_application)
 cpdem_success = emv_success.estimate_cpd(node = 'success')
-print(cpdem_success)
+#print(cpdem_success)
 
 #%% Predecir para la variable de éxito académico
-
-from pgmpy.inference import VariableElimination
 
 #Asociar las CPDs al modelo
 model_success.add_cpds(cpdem_grade_1,
@@ -74,55 +69,59 @@ model_success.add_cpds(cpdem_grade_1,
                        cpdem_success)
 
 #Revisar que el modelo esté completo
-print(model_success.check_model())
-
-#%%
+print("Modelo completo:",model_success.check_model())
 
 # Crear un objeto de inferencia
 inference = VariableElimination(model_success)
-# TODO Existen predicciones que arrojan NaN
 
-# Crear una lista para guardar las predicciones
-predictions = []
+def make_predictions(inference):
 
-for _, row in X_test.iterrows():
+    # Crear una lista para guardar las predicciones
+    predictions = []
 
-    # Crear un diccionario de evidencias con los datos de cada fila en datos de prueba
-    evidence = {variable: row[variable] for variable in row.index if variable!='success'}
-    
-    # Realizar la inferencia para obtener la CPD de "success" dado la evidencia
-    result = inference.query(variables=['success'], evidence=evidence)
-    try:
-    # Agregar la predicción a la lista de predicciones
-        list_result = list(result.values)
-        max_val = list_result.index(max(result.values))
-        if max_val == 0:
-            predictions.append('Dropout')
-        elif max_val == 1:
-            predictions.append('Enrolled')
-        else:
-            predictions.append('Graduate')
-    except:
-        predictions.append('NaN')
+    for _, row in X_test.iterrows():
 
-#%% Obtener metricas
+        # Crear un diccionario de evidencias con los datos de cada fila en datos de prueba
+        evidence = {variable: row[variable] for variable in row.index if variable!='success'}
 
-from sklearn.metrics import accuracy_score, confusion_matrix
+        # Realizar la inferencia para obtener la CPD de "success" dado la evidencia
+        result = inference.query(variables=['success'], evidence=evidence)
+        try:
+        # Agregar la predicción a la lista de predicciones
+            list_result = list(result.values)
+            max_val = list_result.index(max(result.values))
+            if max_val == 0:
+                predictions.append('Dropout')
+            elif max_val == 1:
+                predictions.append('Enrolled')
+            else:
+                predictions.append('Graduate')
+        except:
+            predictions.append('NaN')
 
-# Obtener el ground truth para comparar las predicciones
-y_true = X_test['success'].tolist()
+    # Obtener el ground truth para comparar las predicciones
+    y_true = X_test['success'].tolist()
 
-# Calcular la exactitud del modelo
-accuracy = accuracy_score(y_true, predictions)
-print(f'Exactitud (Accuracy): {accuracy}')
+    # Calcular la exactitud del modelo
+    accuracy = accuracy_score(y_true, predictions)
+    print(f'Exactitud (Accuracy): {accuracy}')
 
-# Calcular la matriz de confusión
-confusion = confusion_matrix(y_true, predictions)
+    # Calcular la matriz de confusión
+    confusion = confusion_matrix(y_true, predictions)
 
-# Extraer los valores de Verdaderos positivos, falsos positivos, falsos negativos y verdaderos negativos.
-# Se entiende como positivo haberse graduado, y negativo seguir matrículado o desertar
-t_drop, drop_as_enr, drop_as_grad, enr_as_drop, t_enr, enr_as_grad, grad_as_drop, grad_as_enr, t_grad = confusion.ravel()
-print(f'Verdaderos Positivos: {sum({t_grad}) }')
-print(f'Falsos Positivos: {sum({drop_as_grad, enr_as_grad})}')
-print(f'Verdaderos Negativos: {sum({t_drop, drop_as_enr, enr_as_drop, t_enr})}')
-print(f'Falsos Negativos: {sum({grad_as_drop, grad_as_enr})}')
+    # Extraer los valores de Verdaderos positivos, falsos positivos, falsos negativos y verdaderos negativos.
+    # Se entiende como positivo haberse graduado, y negativo seguir matrículado o desertar
+    t_drop, drop_as_enr, drop_as_grad, enr_as_drop, t_enr, enr_as_grad, grad_as_drop, grad_as_enr, t_grad = confusion.ravel()
+    true_positives = sum({t_grad})
+    false_positives = sum({drop_as_grad, enr_as_grad})
+    true_negatives = sum({t_drop, drop_as_enr, enr_as_drop, t_enr})
+    false_negatives = sum({grad_as_drop, grad_as_enr})
+
+    return true_positives, false_positives, true_negatives, false_negatives
+
+if __name__ == "__main__":
+    tp, fp, tn, fn = make_predictions(inference)
+    print(f'Verdaderos Positivos: {tp}')
+    print(f'Falsos Positivos: {fp}')
+    print(f'Verdaderos Negativos: {tn}')
+    print(f'Falsos Negativos: {fn}')
